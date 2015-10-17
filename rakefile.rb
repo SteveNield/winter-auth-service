198b2e079d67352c956cb427fdca25b50a396c09
Steve Nield <steve@xevosoftware.com>
2015-10-17 16:11:46 +0100

    Added auth service; auth/login endpoint; tests

---
+++ rakefile.rb
@@ -0,0 +1,33 @@
+require 'albacore'
+
+SOLUTION_DIR = "winter.auth.service.sln"
+PACKAGES_DIR = "packages"
+NUGET_DIR = "c:/program files (x86)/nuget/nuget.exe"
+WEB_PROJECT_BIN_DIR = "winter.auth.service/bin/"
+
+task :default => [:build]
+
+desc 'restore all nugets as per the packages.config files'
+nugets_restore :restore do |p|
+  p.out = PACKAGES_DIR
+  p.exe = NUGET_DIR
+end
+
+desc "Buildling"
+build :build => [:restore] do |b|
+  b.sln = SOLUTION_DIR
+  b.target = ['Clean', 'Rebuild']
+  b.prop 'Configuration', 'Release'
+end
+
+
+desc "Publishing"
+build :publish => [:restore] do |b|
+  b.sln = SOLUTION_DIR
+  b.target = ['Clean', 'Rebuild']
+  b.prop 'Configuration', 'Release'
+  b.prop 'UseWPP_CopyWebApplication', 'true'    # applies the web.config transforms for the build config
+  b.prop 'PipelineDependsOnBuild', 'false'      # makes it so you can package without building first
+  b.prop 'webprojectoutputdir', 'output/'       # the directory to write the published files to
+  b.prop 'outdir', WEB_PROJECT_BIN_DIR          # the directory of your bin files for the project
+end
